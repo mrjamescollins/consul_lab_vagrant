492e6ae8d9642ab6f507291ad53352d032769f80
James Collins <mrjamescollins@users.noreply.github.com>
2019-01-05 18:45:18 -0500

    initial working lab setup
    had to go through a couple of local revisions, had issues with systemctl and the first perms I tried for consul

---
+++ provision.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# update and unzip
+dpkg -s unzip &>/dev/null || {
+	echo 'Installing unzip and doing the update dance.'
+	apt-get -y update && apt-get install -y unzip
+}
+
+
+# install consul
+if [ ! -f /home/vagrant/consul ]; then
+	echo 'Consul was not previously installed.'
+	cd /home/vagrant
+	version='0.8.0'
+	wget https://releases.hashicorp.com/consul/${version}/consul_${version}_linux_amd64.zip -O consul.zip
+	unzip consul.zip
+	rm consul.zip
+
+	# make consul executable
+	chmod +x consul
+fi 
+
+# copy over consul systemd servicefile
+sudo chown vagrant:vagrant consul
+sudo cp consul /usr/local/bin/consul
+cp /vagrant/consul.service /etc/systemd/system/consul.service
+sudo chmod 777 /etc/systemd/system/consul.service
+mkdir -p /etc/systemd/system/consul.d/
+mkdir -p /tmp/consul/
+
+echo 'All done!'
